743bbf56d6edf5048230baf2e8d0742dfab72034
liuweiqing <liuweiqing147@gmail.com>
2024-01-28 15:19:32 +0800

    fix: content length

--- app/api/[...abc]/route.ts
+++ app/api/[...abc]/route.ts
@@ -16,7 +16,7 @@ export async function POST(req: Request) {
     const headers = new Headers(req.headers);
     // 移除或替换可能引起问题的头部
     headers.delete("Host");
-    // headers.delete("Content-Length");
+    headers.delete("Content-Length");
     headers.delete("Upstream-Url"); // 也删除上游 URL 头部，以免发送到上游服务器
     headers.delete("cf-connecting-ip");
     headers.delete("cf-ipcountry");
