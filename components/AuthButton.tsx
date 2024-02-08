2be00c56d311a0d522bee693a3a0c672e9d4e13c
liuweiqing <liuweiqing147@gmail.com>
2024-02-08 10:29:21 +0800

    chore: other

--- components/AuthButton.tsx
+++ components/AuthButton.tsx
@@ -1,7 +1,10 @@
+// "use server";
+
 import { createClient } from "@/utils/supabase/server";
 import Link from "next/link";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
+// import { signOut } from "@/utils/supabase/serversignout";
 
 export default async function AuthButton() {
   const cookieStore = cookies();
@@ -17,15 +20,16 @@ export default async function AuthButton() {
     const cookieStore = cookies();
     const supabase = createClient(cookieStore);
     await supabase.auth.signOut();
+
     return redirect("/login");
   };
 
   return user ? (
     <div className="flex items-center gap-4">
       Hey, {user.email}!
-      <div className="vip-icon bg-yellow-400 text-white p-2 rounded-full shadow-lg animate-pulse">
+      {/* <div className="vip-icon bg-yellow-400 text-white p-2 rounded-full shadow-lg animate-pulse">
         VIP
-      </div>
+      </div> */}
       <form action={signOut}>
         <button className="py-2 px-4 rounded-md no-underline bg-btn-background hover:bg-btn-background-hover">
           Logout
