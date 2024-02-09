84e0363313487020b97d0056c65f4a26f10f4cae
liuweiqing <liuweiqing147@gmail.com>
2024-02-09 23:09:48 +0800

    fix: vercel部署密钥问题

--- utils/supabase/servicerole.ts
+++ utils/supabase/servicerole.ts
@@ -2,7 +2,7 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabaseAdmin = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SECRET_KEY!,
+  process.env.SUPABASE_SECRET_KEY || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
   {
     auth: {
       persistSession: false,
