61576aaa6db62b61749f22fde05856ad808e0ae0
liuweiqing <liuweiqing147@gmail.com>
2024-02-17 11:03:55 +0800

    docs: 文档网站

---
+++ paperai-docs/docs/src/.vuepress/config.js
@@ -0,0 +1,71 @@
+const { description } = require('../../package')
+
+module.exports = {
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#title
+   */
+  title: 'docs of paperai',
+  /**
+   * Ref：https://v1.vuepress.vuejs.org/config/#description
+   */
+  description: description,
+
+  /**
+   * Extra tags to be injected to the page HTML `<head>`
+   *
+   * ref：https://v1.vuepress.vuejs.org/config/#head
+   */
+  head: [
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    [
+      'meta',
+      { name: 'apple-mobile-web-app-status-bar-style', content: 'black' },
+    ],
+  ],
+
+  /**
+   * Theme configuration, here is the default theme configuration for VuePress.
+   *
+   * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+   */
+  themeConfig: {
+    repo: '',
+    editLinks: false,
+    docsDir: '',
+    editLinkText: '',
+    lastUpdated: false,
+    nav: [
+      {
+        text: 'Guide',
+        link: '/guide/',
+      },
+      {
+        text: 'Config',
+        link: '/config/',
+      },
+      {
+        text: 'GitHub',
+        link: 'https://github.com/14790897/paper-ai',
+      },
+    ],
+    sidebar: {
+      '/guide/': [
+        {
+          title: 'Guide2',
+          collapsable: false,
+          children: [
+            '', 
+          'paperai-deploy', 
+          'env'
+        ],
+        },
+      ],
+    },
+  },
+
+  /**
+   * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+   */
+  plugins: ['@vuepress/plugin-back-to-top', '@vuepress/plugin-medium-zoom'],
+}
---
+++ paperai-docs/docs/src/index.md
@@ -0,0 +1,15 @@
+---
+home: true
+# heroImage: https://v1.vuepress.vuejs.org/hero.png
+tagline: docs and usage of paperai
+actionText: Quick Start →
+actionLink: /guide/
+features:
+- title: Feature 1 使用真实文献
+  details: 从各种文献网站获取真实文献
+- title: Feature 2 AI写作
+  details: 内置免费AI模型
+- title: Feature 3 文献管理
+  details: 将文献有序地排列
+footer: Made by liuweiqing with ❤️
+---
