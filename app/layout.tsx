81552993bed272a5a05b19edd41aeb10106124b8
liuweiqing <liuweiqing147@gmail.com>
2024-02-20 22:26:53 +0800

    fix: 用户反馈组件

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,42 +53,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={GeistSans.className}>
-      <Script
-        dangerouslySetInnerHTML={{
-          __html: `
-      // We pre-filled your app ID in the widget URL: 'https://widget.intercom.io/widget/mqoya8yp'
-      (function(){
-        var w = window;
-        var ic = w.Intercom;
-        if (typeof ic === "function") {
-          ic('reattach_activator');
-          ic('update', intercomSettings);
-        } else {
-          var d = document;
-          var i = function() { i.c(arguments); };
-          i.q = [];
-          i.c = function(args) { i.q.push(args); };
-          w.Intercom = i;
-          function l() {
-            var s = d.createElement('script');
-            s.type = 'text/javascript';
-            s.async = true;
-            s.src = 'https://widget.intercom.io/widget/mqoya8yp';
-            var x = d.getElementsByTagName('script')[0];
-            x.parentNode.insertBefore(s, x);
-          }
-          if (d.readyState === 'complete') {
-            l();
-          } else if (w.attachEvent) {
-            w.attachEvent('onload', l);
-          } else {
-            w.addEventListener('load', l, false);
-          }
-        }
-      })();
-    `,
-        }}
-      />
+      <Script src="//fw-cdn.com/11368617/4047428.js" chat="true"></Script>
       <body className="bg-background text-foreground">
         <main className="min-h-screen flex flex-col items-center">
           {children}
