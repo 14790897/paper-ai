b55cf4929a8856e093df076281343674b6047216
liuweiqing <liuweiqing147@gmail.com>
2024-02-20 09:59:48 +0800

    chore: 移动文件

--- middleware.ts
+++ middleware.ts
@@ -69,6 +69,6 @@ export const config = {
      * - favicon.ico (favicon file)
      * Feel free to modify this pattern to include more paths.
      */
-    "/((?!_next/static|_next/image|favicon.ico).*)",
+    "/((?!_next/static|_next/image|favicon.ico|twitter-image.png|opengraph-image.png).*)",
   ],
 };
