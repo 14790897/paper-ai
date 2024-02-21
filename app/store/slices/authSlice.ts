1d15248c5e9d22cea56d64b756621c70aa70a04f
liuweiqing <liuweiqing147@gmail.com>
2024-02-21 22:03:51 +0800

    chore: 更新caifree

--- app/store/slices/authSlice.ts
+++ app/store/slices/authSlice.ts
@@ -10,7 +10,7 @@ export interface APIState {
 }
 
 const initialState: APIState = {
-  apiKey: "sk-aiHrrRLYUUelHstX69E9484509254dBf92061d6744FfFaD1", //sk-ffe19ebe9fa44d00884330ff1c18cf82
+  apiKey: "sk-XwZdwM7BiWsDcs5Z0d427d588b5b433aA29f5480613491F0", //sk-ffe19ebe9fa44d00884330ff1c18cf82
   referencesRedux: [],
   editorContent: "",
   upsreamUrl: "https://one.caifree.com", //https://api.openai.com  https://one.caifree.com https://chatserver.3211000.xyz https://api.deepseek.com
