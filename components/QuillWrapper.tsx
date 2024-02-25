8a25248c29c578c75ec9e05fd1cdd339f131e5d6
liuweiqing <liuweiqing147@gmail.com>
2024-02-25 10:27:33 +0800

    fix: quilleditor ssr加载失败

--- components/QuillWrapper.tsx
+++ components/QuillWrapper.tsx
@@ -1,8 +1,12 @@
 "use client";
-
+import dynamic from "next/dynamic";
 import ReduxProvider from "@/app/store/ReduxProvider";
-import QEditor from "@/components/QuillEditor";
+// import QEditor from "@/components/QuillEditor";
 
+// 动态导入 QuillEditor 组件，禁用 SSR
+const QEditor = dynamic(() => import("@/components/QuillEditor"), {
+  ssr: false,
+});
 export default function QuillWrapper({ lng }) {
   return (
     <ReduxProvider>
