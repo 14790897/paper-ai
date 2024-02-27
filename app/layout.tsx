4c78494e4db895fb862a3290abc97155b1a1db20
liuweiqing <liuweiqing147@gmail.com>
2024-02-27 13:38:39 +0800

    chore: msft clarify

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,6 +67,21 @@ export default function RootLayout({
               });
             }
           `}</Script> */}
+      {/* msft clarify */}
+      <Script>
+        {`(function (c, l, a, r, i, t, y) {
+        c[a] =
+          c[a] ||
+          function () {
+            (c[a].q = c[a].q || []).push(arguments);
+          };
+        t = l.createElement(r);
+        t.async = 1;
+        t.src = "https://www.clarity.ms/tag/" + i;
+        y = l.getElementsByTagName(r)[0];
+        y.parentNode.insertBefore(t, y);
+      })(window, document, "clarity", "script", "l869naiex9");`}
+      </Script>
       <body className="bg-background text-foreground">
         <main className="min-h-screen flex flex-col items-center">
           {children}
