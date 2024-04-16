dba8ec332a01914b236db078bbf5ac81b68b178b
liuweiqing <liuweiqing147@gmail.com>
2024-04-16 14:34:59 +0800

    chore: remove强制设置

--- components/QuillEditor.tsx
+++ components/QuillEditor.tsx
@@ -249,10 +249,10 @@ const QEditor = ({ lng }) => {
     }
   }, []);
   // 强制更新为我设置的API
-  useEffect(() => {
-    dispatch(setApiKey("sk-GHuPUV6ERD8wVmmr36FeB8D809D34d93Bb857c009f6aF9Fe"));
-    dispatch(setUpsreamUrl("https://one.paperai.life"));
-  });
+  // useEffect(() => {
+  //   dispatch(setApiKey("sk-GHuPUV6ERD8wVmmr36FeB8D809D34d93Bb857c009f6aF9Fe"));
+  //   dispatch(setUpsreamUrl("https://one.paperai.life"));
+  // });
   const handleTextChange = debounce(async function (delta, oldDelta, source) {
     if (source === "user") {
       // 获取编辑器内容
